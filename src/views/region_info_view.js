57ad9cd29d223953fa4a1a2f48dbe3707f133dbd
marisol <mtoscanor37@gmail.com>
2019-04-21 17:56:02 +0100

    Finished info view with details for carbon intensity by region

--- src/views/region_info_view.js
+++ src/views/region_info_view.js
@@ -16,17 +16,57 @@ RegionInfoView.prototype.render = function (region) {
   this.container.innerHTML = '';
 
   const heading = this.createHeading(region);
+  const detailsContainer = this.createDetailContainer(region);
+  const fuelsContainer = this.createListOfFuels(region);
 
   this.container.appendChild(heading);
-
+  this.container.appendChild(detailsContainer);
+  this.container.appendChild(fuelsContainer);
 }
 
-RegionInfoView.prototype.createHeading = function (regions) {
-  // console.log('name', regions.shortname);
-  const heading = this.createElement('h2', `${regions.shortname}`);
+RegionInfoView.prototype.createHeading = function (region) {
+  const heading = this.createElement('h2', `${region.shortname}`);
   // console.log("heading", heading);
   return heading;
-}
+};
+RegionInfoView.prototype.createDetailContainer = function (region) {
+  const container = document.createElement('div');
+  container.classList.add('details-box');
+
+  const dnoregion = this.createElement('h3', `${region.dnoregion}`);
+  container.appendChild(dnoregion);
+
+  const carbonIntensity = this.createElement('h4', `${region.intensity.index} - ${region.intensity.forecast} GW` );
+  container.appendChild(carbonIntensity);
+
+  return container;
+};
+
+RegionInfoView.prototype.createListOfFuels = function (region) {
+  const container = document.createElement('div');
+  container.classList.add('fuels-box');
+
+  const list = this.populateListOfFuels(region);
+
+  container.appendChild(list);
+
+  return container;
+};
+
+RegionInfoView.prototype.populateListOfFuels = function (region) {
+  const fuelsList = document.createElement('ul');
+  const regionFuels = region.generationmix;
+
+  regionFuels.forEach( (region, index) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = `${region.fuel} (${region.perc}%)`;
+    listItem.id = index;
+
+    fuelsList.appendChild(listItem);
+  })
+
+  return fuelsList;
+};
 
 RegionInfoView.prototype.createElement = function (elementType, text) {
   const element = document.createElement(elementType);
